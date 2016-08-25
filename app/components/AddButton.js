0fa8217d27d8631feb5e752accc06dc0db5c8554
John Colella <colella.john@gmail.com>
2016-08-25 17:25:22 -0400

    add logic to highlight a clicked document as red; need to figure out logic to keep document highlighted for remainder of session without iterating over entire array of added documents for each render

--- app/components/AddButton.js
+++ app/components/AddButton.js
@@ -2,7 +2,9 @@ var React = require('react');
 
 function AddButton ( props ) {
   return (
-    <button onClick={ function() { props.onUpdateDocumentList( props.documentToAdd ) } }>add document</button>
+    <div>
+      <button className="btn btn-default" onClick={ function() { props.onUpdateDocumentList( props.documentToAdd ) } }>add document</button>
+    </div>
   )
 };
 
--- app/components/Document.js
+++ app/components/Document.js
@@ -1,9 +1,17 @@
 var React = require('react');
 
 function Document ( props ) {
+  debugger;
+  if ( props.documentList.includes( props.data ) || props.documentToAdd && props.documentToAdd.id === props.data.id ) {
+    var render =
+      <h4 id="clicked">{ props.data.name }</h4>
+  } else {
+    var render =
+      <h4 className="pointer" onClick={ function() { props.onShowAddButton( props.data ) } }>{ props.data.name }</h4>
+  }
   return (
     <div>
-      <h4 onClick={ function() { props.onShowAddButton( props.data ) } }>{ props.data.name }</h4>
+      { render }
     </div>
   )
 };
