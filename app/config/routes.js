d177293c9366d22594219a0da5ca121d72b6fc10
John Colella <colella.john@gmail.com>
2016-08-26 12:10:15 -0400

    update routes file for better app organization and flow

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,16 +1,19 @@
 var React = require('react');
 var ReactRouter = require('react-router')
 var Router = ReactRouter.Router;
-var IndexRoute = ReactRouter.IndexRouter;
 var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var AppContainer = require("../containers/AppContainer");
+var PartitionContainer = require("../containers/PartitionContainer");
 var RootFolderContainer = require("../containers/RootFolderContainer");
 
 var routes = (
   <Router history={ hashHistory }>
-    <Route path="/" component={AppContainer}/>
-    <Route path="/partitions/:id" component={RootFolderContainer}/>
+    <Route path="/" component={AppContainer}>
+      <IndexRoute component={PartitionContainer} />
+      <Route path="/partitions/:id" component={RootFolderContainer} />
+    </Route>
   </Router>
 );
 
