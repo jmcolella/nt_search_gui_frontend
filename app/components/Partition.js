03896112ced20db09fbc659bb8fbc1b992a76dce
John Colella <colella.john@gmail.com>
2016-08-26 12:10:44 -0400

    update Partition Container and App Container to reflect new routing

--- app/components/Partition.js
+++ app/components/Partition.js
@@ -1,12 +1,13 @@
 var React = require("react");
+var ReactRouter = require("react-router");
+var Link = ReactRouter.Link;
 
 var Partition = React.createClass({
   render: function() {
     var partition = this.props.data;
-    var onUpdateRender = this.props.onUpdateRender
     return (
       <div className="col-lg-6 col-md-6 col-sm-12">
-        <h1 className="pointer" onClick={ function(){ onUpdateRender("/partitions/" + partition.id) } }><a>{ partition.name }</a></h1>
+        <h1><Link to={ "/partitions/" + partition.id }>{ partition.name }</Link></h1>
       </div>
     )
   }
--- app/containers/PartitionContainer.js
+++ app/containers/PartitionContainer.js
@@ -1,18 +1,33 @@
 var React = require('react');
-var Partition = require('../components/Partition');
+var Partition = require("../components/Partition");
 
 var PartitionContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      partitions: [],
+    }
+  },
+  componentWillMount: function() {
+    $.ajax({
+      url: "http://localhost:3000/partitions",
+      type: "GET"
+    }).done( function( response ) {
+
+      this.setState({
+        partitions: response.partitions,
+      })
+    }.bind(this));
+  },
   render: function () {
     return (
       <div className="row">
         {
-            this.props.partitions.map( function( partition ) {
-              return <Partition
-                        key={ partition.id }
-                        data={ partition }
-                        onUpdateRender={ this.props.onUpdateRender } />
-            }.bind(this))
-          }
+          this.state.partitions.map( function( partition ) {
+            return <Partition
+                      key={ partition.id }
+                      data={ partition } />
+          }.bind(this))
+        }
       </div>
     )
   }
