68dae7ccb556411b248faec64ff86f8cebe63b3b
John Colella <colella.john@gmail.com>
2016-08-30 17:20:26 -0400

    update Bootstrap grid classes for App Container

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -3,7 +3,7 @@ var React = require('react');
 var AppContainer = React.createClass({
   render: function() {
     return (
-      <div className="container-fluid">
+      <div className="container-fluid font">
         <div className="row text-center">
           <h1>NT Search GUI</h1>
         </div>
