15a07588c77bfc94f23be1652c7aa7d8bc1b21ef
jmcolella <colella.john@gmail.com>
2016-11-18 16:25:43 -0500

    finish tests for BreadCrumbContainer

--- app/test/BreadCrumbContainer-test.js
+++ app/test/BreadCrumbContainer-test.js
@@ -1,5 +1,5 @@
 var React = require( 'react' );
-var shallow = require( 'enzyme' ).shallow;
+var mount = require( 'enzyme' ).mount;
 var expect = require( 'chai' ).expect;
 var BreadCrumbContainer = require( '../containers/BreadCrumbContainer' );
 
@@ -8,16 +8,25 @@ describe ( 'Bread Crumb Container', function () {
   var breadCrumbContainer;
 
   beforeEach( function () {
-    breadCrumbContainer = shallow( <BreadCrumbContainer data = { { name: "Test Link" } } collapseData = { {} } /> );
+    breadCrumbContainer = mount( <BreadCrumbContainer data = { { name: "Test Link" } } /> );
   });
 
   it ( 'renders a <li> node with class `.bread-crumb`', function () {
     expect( breadCrumbContainer.find( '.bread-crumb' ) ).to.have.length( 1 );
   });
 
-  it ( 'renders normal name in props', function () {
-    //need to fix this
-    //expect( breadCrumbContainer.find( 'a' ).text() ).to.equal( breadCrumbContainer.props().data.name );
+  it ( 'renders normal name in props if no collapseData', function () {
+    expect( breadCrumbContainer.find( 'a' ).text() ).to.equal( breadCrumbContainer.props().data.name );
+  });
+
+  it ( 'renders `root` in link if name props is `.`', function () {
+    breadCrumbContainer.setProps( { data: {name: "."} } );
+    expect( breadCrumbContainer.find( 'a' ).text() ).to.equal( "root" );
+  });
+
+  it ( 'renders `..` in link if collapseData in props', function () {
+    breadCrumbContainer.setProps( { collapseData: {} } );
+    expect( breadCrumbContainer.find( 'a' ).text() ).to.equal( ".." );
   });
 
 });
